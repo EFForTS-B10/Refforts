c727bf69e56615bb4a819146b3e4837754691f9e
Sebastian Fiedler <sebastian87@online.de>
2023-03-29 14:49:26 +0200

    corrected parameter table

--- data_raw/load_abm_table.R
+++ data_raw/load_abm_table.R
@@ -1,5 +1,5 @@
 
 ## Load csv:
-abm.table <- read.csv("data_raw/EFForTS-ABM_parameters_and_output.csv", header=TRUE, sep=";", stringsAsFactors = FALSE)
+abm.table <- read.csv("data_raw/EFForTS-ABM_parameters_and_output.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 
 usethis::use_data(abm.table, overwrite = TRUE)
